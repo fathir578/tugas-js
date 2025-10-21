f34c8b0a9eb5224d89192e891f09034f6873ac1d
fathir <fathirsyahputra559@gmail.com>
2025-10-21 13:34:24 +0700

    menambahkan file HTML dan JavaScript untuk penjelasan objek di JavaScript

---
+++ pweb/11/objek.js
@@ -0,0 +1,40 @@
+// ============================
+// Membuat sebuah objek
+// ============================
+let mobil = {
+    // ====== PROPERTIES ======
+    // Properti adalah data/atribut yang dimiliki oleh objek
+    merk: "Toyota",
+    warna: "Hitam",
+    tahun: 2020,
+    kecepatan: 0,
+
+    // ====== METHODS ======
+    // Method adalah fungsi (function) yang berada di dalam objek
+    // Method digunakan untuk melakukan aksi pada objek tersebut
+    nyalakanMesin: function() {
+        return "Mesin dinyalakan...";
+    },
+    percepat: function(naik) {
+        this.kecepatan += naik; // 'this' mengacu pada objek 'mobil'
+        return "Kecepatan sekarang: " + this.kecepatan + " km/jam";
+    },
+    infoMobil: function() {
+        return `Mobil ${this.merk} berwarna ${this.warna}, keluaran tahun ${this.tahun}.`;
+    }
+};
+
+// ============================
+// Menampilkan hasil ke halaman
+// ============================
+let output = document.getElementById("output");
+
+output.innerHTML += "<b>Properti Mobil:</b><br>";
+output.innerHTML += "Merk: " + mobil.merk + "<br>";
+output.innerHTML += "Warna: " + mobil.warna + "<br>";
+output.innerHTML += "Tahun: " + mobil.tahun + "<br><br>";
+
+output.innerHTML += "<b>Method Mobil:</b><br>";
+output.innerHTML += mobil.nyalakanMesin() + "<br>";
+output.innerHTML += mobil.percepat(40) + "<br>";
+output.innerHTML += mobil.infoMobil() + "<br>";
